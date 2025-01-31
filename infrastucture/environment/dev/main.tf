813c00190a22e61de56853ab01ac2418d759e41a
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-01-31 13:45:23 +0530

    Update main.tf

--- infrastucture/environment/dev/main.tf
+++ infrastucture/environment/dev/main.tf
@@ -6,18 +6,12 @@ module "vpc" {
   availability_zones   = var.availability_zones
   environment         = var.environment
 }
-module "ecs" {
-  source                  = "../../modules/ecs"
-  public_subnet_ids       = module.vpc.public_subnet_ids
-  ecs_security_group_id   = module.iam.ecs_security_group_id
-  environment             = var.environment
-  execution_role_arn      = module.iam.ecs_execution_role_arn
-  task_role_arn           = module.iam.ecs_task_role_arn
-  ecr_patient_repo_url    = module.ecr.patient_service_repo_url
-  ecr_appointment_repo_url = module.ecr.appointment_service_repo_url
-  patient_tg_arn      = module.alb.patient_tg_arn
-  appointment_tg_arn  = module.alb.appointment_tg_arn
 
+module "eks" {
+  source = "../../modules/eks"
+  cluster_name = "dev-eks-cluster"
+  cluster_role_arn = module.iam.eks_cluster_role_arn
+  subnet_ids = module.vpc.private_subnets
 }
 
 module "iam" {
@@ -33,10 +27,4 @@ module "ecr" {
   environment = var.environment
 }
 
-module "alb" {
-  source                = "../../modules/alb"
-  alb_security_group_id     = module.iam.alb_security_group_id
-  public_subnet_ids      = module.vpc.public_subnet_ids
-  vpc_id                = module.vpc.vpc_id
-  environment           = var.environment
-}
+
