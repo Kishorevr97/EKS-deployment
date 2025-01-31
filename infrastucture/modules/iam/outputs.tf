4b056bab905153021daa7afa990c001d27fcf8c6
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-01-31 17:18:42 +0530

    Update outputs.tf

--- infrastucture/modules/iam/outputs.tf
+++ infrastucture/modules/iam/outputs.tf
@@ -1,16 +1,7 @@
-output "eks_security_group_id" {
-  value = aws_security_group.eks_security_group.id
-}
-
-
-output "eks_execution_role_arn" {
-  value = aws_iam_role.eks_execution_role.arn
-}
-
-output "eks_task_role_arn" {
-  value = aws_iam_role.eks_task_role.arn
-}
-
 output "eks_cluster_role_arn" {
   value = aws_iam_role.eks_cluster_role.arn
+}
+
+output "fargate_pod_execution_role_arn" {
+  value = aws_iam_role.fargate_pod_execution_role.arn
 }
