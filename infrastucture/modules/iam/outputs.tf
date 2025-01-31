3a02d19c422b002bca374d13eebab861d3102f66
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-01-31 13:42:12 +0530

    Update outputs.tf

--- infrastucture/modules/iam/outputs.tf
+++ infrastucture/modules/iam/outputs.tf
@@ -1,15 +1,12 @@
-output "ecs_security_group_id" {
-  value = aws_security_group.ecs_security_group.id
+output "eks_security_group_id" {
+  value = aws_security_group.eks_security_group.id
 }
 
-output "alb_security_group_id" {
-  value = aws_security_group.alb_security_group.id
+
+output "eks_execution_role_arn" {
+  value = aws_iam_role.eks_execution_role.arn
 }
 
-output "ecs_execution_role_arn" {
-  value = aws_iam_role.ecs_execution_role.arn
-}
-
-output "ecs_task_role_arn" {
-  value = aws_iam_role.ecs_task_role.arn
+output "eks_task_role_arn" {
+  value = aws_iam_role.eks_task_role.arn
 }
