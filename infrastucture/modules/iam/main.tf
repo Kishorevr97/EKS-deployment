a58a36e7ee45d469181d722b2136920563678a36
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-01-31 14:24:23 +0530

    Update main.tf

--- infrastucture/modules/iam/main.tf
+++ infrastucture/modules/iam/main.tf
@@ -1,16 +1,3 @@
-resource "aws_iam_role" "eks_execution_role" {
-  name = "${var.environment}-eks-execution-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Principal = {
-        Service = "eks-tasks.amazonaws.com"
-      }
-      Effect = "Allow"
-    }]
-  })
-}
 
 resource "aws_iam_role" "eks_task_role" {
   name = "${var.environment}-eks-task-role"
