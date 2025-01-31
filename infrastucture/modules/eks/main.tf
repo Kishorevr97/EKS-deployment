e32a1a91bbacbd6482e515450c3922927d895066
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-01-31 16:54:44 +0530

    Update main.tf

--- infrastucture/modules/eks/main.tf
+++ infrastucture/modules/eks/main.tf
@@ -1,11 +1,28 @@
-resource "aws_eks_cluster" "eks_cluster" {
-  name = var.cluster_name
+resource "aws_eks_cluster" "main" {
+  name     = var.cluster_name
   role_arn = var.cluster_role_arn
   vpc_config {
-    subnet_ids = var.subnet_ids
+    subnet_ids = var.private_subnet_ids
+    endpoint_public_access = true
+    endpoint_private_access = true
   }
 
+  # Add additional tags or configurations as required
   tags = {
-    Name = var.cluster_name
+    Name = "eks-cluster"
   }
+}
+
+# Fargate Profile for EKS
+resource "aws_eks_fargate_profile" "main" {
+  cluster_name = aws_eks_cluster.main.name
+  fargate_profile_name = var.fargate_profile_name
+  pod_execution_role_arn = var.fargate_role_arn
+
+  selector {
+    namespace = var.fargate_namespace
+  }
+
+  # Optionally, you can associate specific subnets to the Fargate profile
+  subnet_ids = var.private_subnet_ids
 }
