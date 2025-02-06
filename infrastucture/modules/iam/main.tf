46e88ab6d08ce035d1682b19a4fcc0631df2af81
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-02-06 19:22:51 +0530

    Update main.tf

--- infrastucture/modules/iam/main.tf
+++ infrastucture/modules/iam/main.tf
@@ -1,3 +1,4 @@
+# IAM Role for the EKS Cluster
 resource "aws_iam_role" "eks_cluster_role" {
   name = var.cluster_role_name
 
@@ -10,11 +11,49 @@ resource "aws_iam_role" "eks_cluster_role" {
           Service = "eks.amazonaws.com"
         }
         Effect = "Allow"
-        Sid    = ""
       },
     ]
   })
 }
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# IAM Role for Worker Nodes
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-group-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+# IAM Role Policy Attachments for Worker Nodes
+resource "aws_iam_role_policy_attachment" "eks_worker_node" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_container_registry" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+
+
 
 # IAM Role for Fargate Pod Execution
 /*resource "aws_iam_role" "fargate_pod_execution_role" {
