13d8847bc58ac06f242c74f519c55184dc696656
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-02-06 19:24:25 +0530

    Update outputs.tf

--- infrastucture/modules/iam/outputs.tf
+++ infrastucture/modules/iam/outputs.tf
@@ -2,6 +2,18 @@ output "eks_cluster_role_arn" {
   value = aws_iam_role.eks_cluster_role.arn
 }
 
-output "fargate_pod_execution_role_arn" {
-  value = aws_iam_role.fargate_pod_execution_role.arn
+output "eks_node_role_arn" {
+  description = "ARN of the IAM role for the worker nodes"
+  value       = aws_iam_role.eks_node_role.arn
 }
+output "eks_worker_node_policy_attachment" {
+  value = aws_iam_role_policy_attachment.eks_worker_node
+}
+
+output "eks_cni_policy_attachment" {
+  value = aws_iam_role_policy_attachment.eks_cni_policy
+}
+
+/*output "fargate_pod_execution_role_arn" {
+  value = aws_iam_role.fargate_pod_execution_role.arn
+}*/
