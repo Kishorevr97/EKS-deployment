1d6db557054f80e3855de645d5a1168ea84fe562
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-02-07 14:35:00 +0530

    Delete infrastucture/modules/ecr/variables.tf

--- infrastucture/modules/ecr/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "repository_name" {
-  description = "The name of the ECR repository"
-  type = string
-}
-
-variable "environment" {
-  description = "Environment name (e.g., dev, staging, prod)"
-  type = string
-}
