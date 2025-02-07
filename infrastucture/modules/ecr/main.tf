cdbdc0920df65c9882e92ff4068692fdba3d2825
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-02-07 14:34:11 +0530

    Update main.tf

--- infrastucture/modules/ecr/main.tf
+++ infrastucture/modules/ecr/main.tf
@@ -1,7 +1,13 @@
-resource "aws_ecr_repository" "patient_service_repo" {
-  name = "${var.environment}-patient-service-repo"
+resource "aws_ecr_repository" "patient_service" {
+  name = "patient-service-repo"
+  tags = {
+    Name = "Patient Service Repository"
+  }
 }
 
-resource "aws_ecr_repository" "appointment_service_repo" {
-  name = "${var.environment}-appointment-service-repo"
+resource "aws_ecr_repository" "appointment_service" {
+  name = "appointment-service-repo"
+  tags = {
+    Name = "Appointment Service Repository"
+  }
 }
