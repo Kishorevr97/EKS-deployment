87f4ee8520bf2ae17a5111ac9359140a06970262
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-02-07 14:39:43 +0530

    Update variables.tf

--- infrastucture/modules/vpc/variables.tf
+++ infrastucture/modules/vpc/variables.tf
@@ -1,24 +1,19 @@
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type = string
+variable "cidr_block" {
+  type        = string
+  description = "The CIDR block for the VPC."
 }
 
-variable "public_subnet_cidr" {
-  description = "CIDR block for the public subnet"
-  type = list(string)
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
 }
 
-variable "private_subnet_cidr" {
-  description = "CIDR block for the private subnet"
-  type = list(string)
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
 }
 
-variable "availability_zones" {
-  description = "Availability zone for the subnets"
-  type = list(string)
-}
-
-variable "environment" {
-  description = "Environment name (e.g., dev, staging, prod)"
-  type = string
+variable "availability_zone" {
+  description = "List of availability zones"
+  type        = list(string)
 }
