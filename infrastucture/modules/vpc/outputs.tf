692219102db9b36fe615790d9d2bf15626caa0d8
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-02-07 14:39:19 +0530

    Update outputs.tf

--- infrastucture/modules/vpc/outputs.tf
+++ infrastucture/modules/vpc/outputs.tf
@@ -2,10 +2,19 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
-output "public_subnet_ids" {
-  value = aws_subnet.public_subnet[*].id
-}
 
 output "private_subnet_ids" {
-  value = aws_subnet.private_subnet[*].id
+  value = aws_subnet.private[*].id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "internet_gateway_id" {
+  value = aws_internet_gateway.main.id
+}
+
+output "nat_gateway_id" {
+  value = aws_nat_gateway.main.id
 }
