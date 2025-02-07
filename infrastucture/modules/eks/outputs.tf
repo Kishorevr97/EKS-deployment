4880181419d26e3469d22a719dcca1c3f96a4e8a
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-02-07 14:35:58 +0530

    Update outputs.tf

--- infrastucture/modules/eks/outputs.tf
+++ infrastucture/modules/eks/outputs.tf
@@ -1,4 +1,4 @@
-
+# Output EKS Cluster and Fargate Profile details
 output "eks_cluster_name" {
   value = aws_eks_cluster.main.name
 }
@@ -6,7 +6,3 @@ output "eks_cluster_name" {
 output "eks_cluster_endpoint" {
   value = aws_eks_cluster.main.endpoint
 }
-
-/*output "fargate_profile_name" {
-  value = aws_eks_fargate_profile.main.fargate_profile_name
-}*/
