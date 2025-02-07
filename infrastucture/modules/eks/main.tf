6b37a05578e95844a3433cfa0797c3c38bb96ece
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-02-07 14:01:21 +0530

    Update main.tf

--- infrastucture/modules/eks/main.tf
+++ infrastucture/modules/eks/main.tf
@@ -3,6 +3,7 @@ resource "aws_eks_cluster" "main" {
   role_arn = var.cluster_role_arn
   vpc_config {
     subnet_ids = var.private_subnet_ids
+    security_group_ids = [aws_security_group.eks_sg.id]
     endpoint_public_access = true
     endpoint_private_access = true
   }
