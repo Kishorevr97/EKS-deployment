9053748de9166f86bbee96258b2cde5661a3e272
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-02-07 18:49:17 +0530

    Update main.tf

--- infrastucture/environment/dev/main.tf
+++ infrastucture/environment/dev/main.tf
@@ -56,3 +56,11 @@ output "appointment_service_repo_uri" {
   value = module.ecr.appointment_service_repo_uri
 }
 
+
+module "monitoring" {
+  source = "../../modules/monitoring"
+  eks_cluster_id                     = module.eks.eks_cluster_id
+  eks_cluster_endpoint                = module.eks.eks_cluster_endpoint
+  eks_cluster_certificate_authority   = module.eks.eks_cluster_certificate_authority
+}
+
