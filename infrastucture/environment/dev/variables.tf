0ec76cd28b17f2237d7543c94601e404340e09cc
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-02-07 11:16:36 +0530

    Update variables.tf

--- infrastucture/environment/dev/variables.tf
+++ infrastucture/environment/dev/variables.tf
@@ -35,8 +35,3 @@ variable "repository_name" {
   type        = string
 }
 
-
-variable "vpc_id" {
-  description = "The ID of the VPC where the EKS cluster will be deployed."
-  type        = string
-}
