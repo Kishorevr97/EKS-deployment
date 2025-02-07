19db031f0d287949df5d6df863782b9f1911b862
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-02-07 14:34:49 +0530

    Update outputs.tf

--- infrastucture/modules/ecr/outputs.tf
+++ infrastucture/modules/ecr/outputs.tf
@@ -1,7 +1,8 @@
-output "patient_service_repo_url" {
-  value = aws_ecr_repository.patient_service_repo.repository_url
+# Outputs for ECR Repository URIs
+output "patient_service_repo_uri" {
+  value = aws_ecr_repository.patient_service.repository_url
 }
 
-output "appointment_service_repo_url" {
-  value = aws_ecr_repository.appointment_service_repo.repository_url
+output "appointment_service_repo_uri" {
+  value = aws_ecr_repository.appointment_service.repository_url
 }
