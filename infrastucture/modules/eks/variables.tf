1ed3c7786c65fb84fd2bb0e2b7afd6b997f2cfe4
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-02-07 14:36:59 +0530

    Update variables.tf

--- infrastucture/modules/eks/variables.tf
+++ infrastucture/modules/eks/variables.tf
@@ -1,17 +1,20 @@
+
 variable "cluster_name" {
   type        = string
   description = "The name of the EKS cluster."
 }
 
-variable "cluster_role_arn" {
+variable "eks_cluster_role_arn" {
+  description = "IAM Role ARN for the EKS cluster"
   type        = string
-  description = "The ARN of the EKS cluster role."
 }
+
 
 variable "private_subnet_ids" {
   type        = list(string)
   description = "A list of subnet IDs for the EKS cluster."
 }
+
 
 variable "ssh_key_name" {
   description = "SSH Key for accessing EC2 nodes"
@@ -27,23 +30,3 @@ variable "vpc_id" {
   description = "The ID of the VPC where the EKS cluster will be deployed."
   type        = string
 }
-
-
-
-/*variable "fargate_profile_name" {
-  type        = string
-  description = "The name of the Fargate profile."
-}
-
-variable "fargate_role_arn" {
-  type        = string
-  description = "The ARN of the Fargate pod execution role."
-}
-
-variable "fargate_namespace" {
-  type        = string
-  description = "The Kubernetes namespace for Fargate."
-}*/
-
-
-
